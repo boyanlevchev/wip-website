2e6ff9e087fde12bf3db89779ff31d5fc9be0e0c
Boyan Levchev <boyanlevchev@gmail.com>
2021-01-21 21:08:26 +0200

    Initial version of website

---
+++ src/pages/interview_transcriptions.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function InterviewTranscriptions(props) {
+
+  return(
+    <div className={"margins"}>
+      <h1>Julie transcribing - Extracts</h1>
+
+      <section>
+        <h2>Session 2</h2>
+        <ul className={"no-bullets"}>
+          <li className={"blue-text"}>-smell of food, smelling garlic & energy to make it tasty</li>
+          <li>-That’s the thing with travel, you pause much more to realise things, because it’s not part of your routine. There is something so special - you pay much more attention. It did feel really special.</li>
+          <li className={"blue-text"}>-I can still visualise it a bit, it’s going away. I don’t think it’s that precise anymore.</li>
+          <li className={"blue-text"}>-Just happy in a way that feels really free.</li>
+          <li className={"blue-text"}>-There is no story, there is no past and no future - it's a suspended time, so it makes stuff really possible</li>
+          <li>-When I’m not feeling good, I’m mainly in remote, thinking about other stuff. I rarely picture open wide spaces.</li>
+          <li>-I’m really good at loopholes, doing loopholes about past relationships. It’s a great space of pain, so I don't need to look too far - it's just there!</li>
+          <li>-During long phases of time where I was not feeling good it’s really hard to pick like, even a day.</li>
+          <li>-I feel like everyone - you’re everywhere but not in the right now (in reference to how time is experienced). Not feeling good is also the incapacity to be in the present, you’re somewhere else, but you’re not in the present - you’re so far.</li>
+          <li>-It’s just this blurry period of time. I imagine a lot of the time when this happens I’m a lot on a sofa or a bed.</li>
+          <li>-My anxiety was too high so I had to go to a bar.</li>
+          <li>-It was a full year where I don’t know what I’ve done for a full year. I have no idea. It’s really hard to remember.</li>
+          <li className={"blue-text"}>43,59 - the only thing I have from this time is little pieces of writing. But who wants to read that. You always describe the same sensation. For me it's very physical, feeling something in your stomach.</li>
+          <li>-The feeling now is much more about anxiety, there’s this feeling of emptiness which is related to something else. It feels really physical. There is a huge hole in the middle.</li>
+          <li className={"blue-text"}>-I know it was bad but I don’t really remember it.</li>
+          <li className={"blue-text"}>-If I search I can remember the thought process (48,30)</li>
+          <li>-Even if you know so many people feel similar, it's so hard to share with anyone. It’s not really intelligible because it is a sensation based on thoughts, not rational thoughts.</li>
+          <li>-It’s like what people say, when you give birth you have to forget the pain. It’s probably the same process - you kind of have to forget the whole process otherwise you wouldn’t be able to move forward.</li>
+          <li className={"blue-text"}>-55,30 You also feel very privileged to recognise there’s a problem.</li>
+          <li>-This situation would happen again today, but maybe it will and maybe I’m wrong it will happen again, but something similar will happen as strong. All the elements</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Session 3</h2>
+        <ul className={"no-bullets"}>
+          <li>-absolute desire for something where it’s pushed to destruction</li>
+          <li>-sensation of something stronger, something so uncontrollable and abstract that feels like taking over.</li>
+          <li className={"blue-text"}>-This feeling and just the difficulty to get out of loops. It’s probably easier to think it's an invader. I often say ’shit, this thing is back’ - it’s coming back. At least I can recognise something. Suddenly the reality changed and nobody else can see it, nobody can understand it. (1.33,29)</li>
+          <li>-Often the main thing is feeling extremely tired, starting having loopy thoughts. Feeling like I just want to stay home for a few days. Just feeling like a very heavy sad feeling. Everything is a bit difficult.</li>
+          <li className={"blue-text"}>-much easier to recognise it now, making it easier in general.</li>
+          <li>It just kind of vanishes. I don’t think about it.</li>
+          <li>-01.38.45 : I’m always surprised when it comes back and always terrified. It’s always kind of scary.</li>
+          <li>-It’s everything suddenly. There is nothing else, you just feel this way and everything becomes difficult. Interacting with people is difficult and doing your job if you have a job is difficult. (01,39.10) You have no idea how long it’s going to last for, if it’s going to be a day, a week or if it will need more than that how long it will last for. Whenever it’s not there you feel like, “Oh, I moved on”. I recognise it more, and when you're in it’s like nothing has changed, I haven’t solved anything. And every time there’s a wake up time, like I haven’t done the work I should have done. It’s really hard to accept that’s what’s going to be. (…) and you hope you’re in an environment and surrounded by people who can accept that this happened sometimes and there’s not much to do about it.</li>
+          <li className={"blue-text"}>-anything that can just switch it off.</li>
+          <li className={"blue-text"}>-for me it was a lot of crying and doing the minimum possible (01.43,29)</li>
+          <li>-I think most people don’t really understand what's going on with you. They will accept it but don’t understand.</li>
+          <li>-in London a lot of people - most people, as I experienced it - at the time were alienated by my emotions.</li>
+          <li className={"blue-text"}>-its the snake situation where you try to express something which takes you a lot of energy and courage and you feel rejected but rejection is a trigger to depression so you just go back deeper. You have to express, otherwise you’re acting out constantly. If I think about this phase, it felt really lonely. It felt really lonely.</li>
+        </ul>
+      </section>
+
+    </div>
+  );
+}
+
+export default InterviewTranscriptions;
