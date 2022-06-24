db9b6b32c6fbd5726a27b32ff1998e035432a5c8
boyanlevchev <boyanlevchev@gmail.com>
2022-06-24 16:24:00 +0100

    Minor bug fixes - better on mobile

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,12 +8,12 @@ function Contact(props) {
 
   return (
     <article className={"page-padding"}>
-      <section>
-        <p>
+      <section className={"text-column"}>
+        <p className={"flex align-center"}>
           <FontAwesomeIcon icon={faEnvelope} className={"contact-icons"}/> julie.dusuel (at) network.rca.ac.uk
         </p>
-        <p>
-          <FontAwesomeIcon icon={faInstagram} className={"contact-icons"} /> @julie.dusuel
+        <p className={"flex align-center"}>
+          <FontAwesomeIcon icon={faInstagram} className={"contact-icons"} /> <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/julie.dusuel/">@julie.dusuel</a>
         </p>
       </section>
     </article>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Home(props) {
 
   return(
-    <div className={"flex w-100 pt-3 justify-center"}>
+    <div className={"flex w-100 pt-3"}>
       <img
-        className="img-size"
+        className="img-size w-100"
         src="https://res.cloudinary.com/dt47helyp/image/upload/v1656027824/Julie/IMG_1810_b2it5y.jpg"
         alt="degree show invitation" />
     </div>
